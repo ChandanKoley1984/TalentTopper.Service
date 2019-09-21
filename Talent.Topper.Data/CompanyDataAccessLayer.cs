c48354bdff48315a4e95e08ab6d4006366c4b608
subhasis <subhasis2020@gmail.com>
2019-09-21 22:06:58 +0530

    commited frm bash using stash

--- Talent.Topper.Data/CompanyDataAccessLayer.cs
+++ Talent.Topper.Data/CompanyDataAccessLayer.cs
@@ -94,13 +94,7 @@ namespace Talent.Topper.Data
 
 
         public bool CreateCompanyWithBranch(CompanyEntity _CompanyEntity)
-        {
-            Dictionary<string, string> _CompanyOutPut = new Dictionary<string, string>();
-            Dictionary<string, string> _BranchOutPut = new Dictionary<string, string>();
-            Dictionary<string, string> _ContactOutPut = new Dictionary<string, string>();
-
-            BranchDataAccessLayer branchDataAccessLayer = new BranchDataAccessLayer();
-            ContactDataAccessLayer contactDataAccessLayer = new ContactDataAccessLayer();
+        {           
 
             COMPANY _company = new COMPANY();
             _company.ID = _CompanyEntity.ID;
@@ -113,90 +107,93 @@ namespace Talent.Topper.Data
             _company.CreatedBy = _CompanyEntity.CreatedBy;
             _company.CompanyType = _CompanyEntity.CompanyType;
 
+            _dbContext.COMPANies.Add(_company);
+            if (_company.ID > 0)
+                _dbContext.Entry(_company).State = EntityState.Modified;
 
 
-            _CompanyOutPut = CreateCompany(_company);
+            // _CompanyOutPut = CreateCompany(_company);
 
-            int saveStatusCompany = Convert.ToUInt16(_CompanyOutPut["saveStatus"]);
+            //int saveStatusCompany = Convert.ToUInt16(_CompanyOutPut["saveStatus"]);
 
-            if (saveStatusCompany > 0)
-            {
+            //if (saveStatusCompany > 0)
+            //{
 
-                BRANCH _branch = new BRANCH();
-                _branch.ID = 0;
-                _branch.Name = _CompanyEntity.CompanyName;
-                _branch.HODName = _CompanyEntity.CEOName;
-                _branch.LogoPath = _CompanyEntity.LogoPath;
-                _branch.IsActive = _CompanyEntity.IsActive;
-                _branch.CreatedDate = _CompanyEntity.CreatedDate;
-                _branch.CreatedBy = _CompanyEntity.CreatedBy;
-                _branch.Company_ID = Convert.ToUInt16(_CompanyOutPut["OutPut"]);
+            BRANCH _branch = new BRANCH();
+            _branch.ID = 0;
+            _branch.Name = _CompanyEntity.CompanyName;
+            _branch.HODName = _CompanyEntity.CEOName;
+            _branch.LogoPath = _CompanyEntity.LogoPath;
+            _branch.IsActive = _CompanyEntity.IsActive;
+            _branch.CreatedDate = _CompanyEntity.CreatedDate;
+            _branch.CreatedBy = _CompanyEntity.CreatedBy;
+            _branch.Company_ID = _company.ID;
+            //_branch.Company_ID = Convert.ToUInt16(_CompanyOutPut["OutPut"]);
 
+            _dbContext.BRANCHes.Add(_branch);
+            if (_branch.ID > 0)
+                _dbContext.Entry(_branch).State = EntityState.Modified;
 
-                _BranchOutPut = branchDataAccessLayer.CreateBranch(_branch);
+            //_BranchOutPut = branchDataAccessLayer.CreateBranch(_branch);
 
-                int saveStatusBranch = Convert.ToUInt16(_BranchOutPut["saveStatus"]);
+            //int saveStatusBranch = Convert.ToUInt16(_BranchOutPut["saveStatus"]);
 
-                if (saveStatusBranch > 0)
-                {
-                    CONTACT _contact = new CONTACT();
-                    _contact.ID = 0;
-                    _contact.Name = _CompanyEntity.CompanyName;
-                    _contact.RoleId = 1;
-                    _contact.MobileNo = _CompanyEntity.MobileNo;
-                    _contact.PhoneNo = _CompanyEntity.PhoneNo;
-                    _contact.Email = _CompanyEntity.Email;
-                    _contact.Password = _CompanyEntity.Password;
-                    _contact.IsActive = _CompanyEntity.IsActive;
-                    _contact.CreatedDate = _CompanyEntity.CreatedDate;
-                    _contact.CreatedBy = _CompanyEntity.CreatedBy;
-                    _contact.Company_ID = Convert.ToUInt16(_CompanyOutPut["OutPut"]);
-                    _contact.Branch_ID = Convert.ToUInt16(_BranchOutPut["OutPut"]);
+            //if (saveStatusBranch > 0)
+            //{
+            CONTACT _contact = new CONTACT();
+            _contact.ID = 0;
+            _contact.Name = _CompanyEntity.CompanyName;
+            _contact.RoleId = 1;
+            _contact.MobileNo = _CompanyEntity.MobileNo;
+            _contact.PhoneNo = _CompanyEntity.PhoneNo;
+            _contact.Email = _CompanyEntity.Email;
+            _contact.Password = _CompanyEntity.Password;
+            _contact.IsActive = _CompanyEntity.IsActive;
+            _contact.CreatedDate = _CompanyEntity.CreatedDate;
+            _contact.CreatedBy = _CompanyEntity.CreatedBy;
 
+            _contact.Company_ID = _company.ID;
+            _contact.Branch_ID = _branch.ID;
 
+            //_contact.Company_ID = Convert.ToUInt16(_CompanyOutPut["OutPut"]);
+            //_contact.Branch_ID = Convert.ToUInt16(_BranchOutPut["OutPut"]);
 
-                    _ContactOutPut = contactDataAccessLayer.CreateContact(_contact);
+            _dbContext.CONTACTs.Add(_contact);
+            if (_contact.ID > 0)
+                _dbContext.Entry(_contact).State = EntityState.Modified;
 
-                    int saveStatusContact = Convert.ToUInt16(_BranchOutPut["saveStatus"]);
+            // _ContactOutPut = contactDataAccessLayer.CreateContact(_contact);
 
-                    if (saveStatusContact > 0)
-                    {
+            //int saveStatusContact = Convert.ToUInt16(_BranchOutPut["saveStatus"]);
 
-                        //*************to update branch and company tablw with contact id 
+            //if (saveStatusContact > 0)
+            //{
 
-                        TalentTopperEntities _dbContext = new TalentTopperEntities();
-                        int Branch_ID = Convert.ToUInt16(_BranchOutPut["OutPut"]);
-                        int Company_ID = Convert.ToUInt16(_CompanyOutPut["OutPut"]);
+            //*************to update branch and company tablw with contact id 
 
-                        var branchbyid = _dbContext.BRANCHes.Where(x => x.ID == Branch_ID).FirstOrDefault();
-
-                        if (branchbyid != null)
-                        {
-                            branchbyid.Contact_Id = Convert.ToUInt16(_ContactOutPut["OutPut"]);
-
-                            _dbContext.Entry(branchbyid).State = EntityState.Modified;
-                            _dbContext.SaveChanges();
-                        }
+            //TalentTopperEntities _dbContext = new TalentTopperEntities();
 
 
-                        var companybyid = _dbContext.COMPANies.Where(x => x.ID == Company_ID).FirstOrDefault();
 
-                        if (companybyid != null)
-                        {
-                            companybyid.Contact_Id = Convert.ToUInt16(_ContactOutPut["OutPut"]);
+            //_branch.Contact_Id = Convert.ToUInt16(_contact.ID);
+            //_dbContext.Entry(_branch).State = EntityState.Modified;
 
-                            _dbContext.Entry(companybyid).State = EntityState.Modified;
-                            _dbContext.SaveChanges();
-                        }
 
-                    }
+            //_company.Contact_Id = Convert.ToUInt16(_contact.ID);
+            //_dbContext.Entry(_company).State = EntityState.Modified;
 
-                }
+            _dbContext.SaveChanges();
+            //}
 
-            }
+            //}
+
+            //}
+
+            //}
             return true;
 
         }
+
     }
-   
+
 }
